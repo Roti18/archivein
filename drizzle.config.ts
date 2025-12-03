49715a8a857026bad9c8de8dbd9ce966a88fa85d
Roti18 <akuronif@gmail.com>
2025-12-03 16:17:54 +0700

    add: local and production settings

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,40 @@
+// LOCAL
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
+const dbUrl = process.env.DATABASE_URL || 'file:development.db';
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-	dialect: 'turso',
+	dialect: 'sqlite',
 	dbCredentials: {
-		authToken: process.env.DATABASE_AUTH_TOKEN,
-		url: process.env.DATABASE_URL
+		// authToken: process.env.DATABASE_AUTH_TOKEN,
+		url: dbUrl
 	},
 	verbose: true,
 	strict: true
 });
+
+// PRODUCTION
+// import { defineConfig } from 'drizzle-kit';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// const dbUrl = process.env.DATABASE_URL;
+// const authToken = process.env.DATABASE_AUTH_TOKEN;
+
+// if (!dbUrl) {
+//     throw new Error('DATABASE_URL is not set in environment variables.');
+// }
+
+// export default defineConfig({
+//     schema: './src/lib/server/db/schema.ts',
+//     dialect: 'sqlite',
+//     dbCredentials: {
+//         authToken: authToken,
+//         url: dbUrl
+//     },
+//     verbose: true,
+//     strict: true
+// });
